5165c2bf57823886003ed0456d64bc72ccc5a4d8
Anna Gladzińska <agladzinska@gmail.com>
2020-01-06 16:32:32 +0100

    Add flex grid, rwd menu

--- js/script.js
+++ js/script.js
@@ -10,17 +10,22 @@ for (let tab of tabs) {
 
 // hamburger
 
-const hamburger = document.querySelector('.hamburger');
-const hamburgerIcon = document.querySelector('.fa-bars');
-const hamburgerIconX = document.querySelector('.fa-times');
-// const sidebarMenu = document.querySelector('.sidebar ul');
-// const sidebarManager = document.querySelector('.manager');
+const hamburgers = document.querySelectorAll('.hamburger');
+const hamburgerIcon = document.querySelectorAll('.hamburger .fa-bars');
+const hamburgerIconX = document.querySelectorAll('.hamburger .fa-times');
+const sidebar = document.querySelector('.sidebar');
 
+for (let hamburger of hamburgers) {
+  hamburger.addEventListener('click', function () {
+    sidebar.classList.toggle('active');
+    sidebar.classList.toggle('unactive');
 
-hamburger.addEventListener('click', function () {
-  hamburgerIcon.classList.toggle('i-active');
-  hamburgerIconX.classList.toggle('i-active');
-  // sidebarMenu.classList.toggle('unactive');
-  // sidebarManager.classList.toggle('unactive');
-});
+    for (let icon of hamburgerIcon) {
+      icon.classList.toggle('i-active');
+    }
 
+    for (let iconX of hamburgerIconX) {
+      iconX.classList.toggle('i-active');
+    }
+  });
+}
